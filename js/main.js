1c6559ebce891b55a1c9eb99013ce89389e9a802
Yulduz <yulduz.karshiyeva@mail.ru>
2024-04-25 14:39:44 +0500

    js media.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const elBtn = document.querySelector('.team-about__more')
+console.log(elBtn)
+elBtn.addEventListener('click',()=>{
+    elBtn.classList.toggle('active')
+})
+
+elBtn.addEventListener('mouseover', () => {
+    elBtn.style.background = 
+})
